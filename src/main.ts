ca3a19f33d4e1ca7faf8b78c9b49a726c0fda880
ioolliver <a>
2024-07-14 09:51:23 -0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ZodFilter } from './zod/filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalFilters(new ZodFilter());
   await app.listen(process.env.PORT ?? 3333);
 }
