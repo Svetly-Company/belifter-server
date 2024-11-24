9ae16bdf7417863ab5534a907b9095c1414d1a1b
ioolliver <101019491+ioolliver@users.noreply.github.com>
2024-11-24 17:29:02 -0300

    feat: upload new

--- src/controllers/upload.controller.ts
+++ src/controllers/upload.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Controller, MaxFileSizeValidator, ParseFilePipe, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express"; 
 import { Public } from "src/auth/public-key";
@@ -17,5 +18,18 @@ export class UploadController {
     })) file: Express.Multer.File) {
         const filename = await this.uploadService.upload(file.buffer);
         return filename;
+    }
+
+    @Post('sendlink')
+    @Public()
+    @UseInterceptors(FileInterceptor('file'))
+    async uploadPostMedia2(@UploadedFile(new ParseFilePipe({
+        validators: [
+            new MaxFileSizeValidator({ maxSize: 3000000 })
+        ]
+    })) file: Express.Multer.File) {
+        const filename = await this.uploadService.upload(file.buffer);
+        const imgUrl = await this.uploadService.getImageUrl(filename);
+        return {filename, imgUrl};
     }
 }
